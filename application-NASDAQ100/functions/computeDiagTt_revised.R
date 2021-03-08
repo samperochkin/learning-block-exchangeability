3bb21078955a74689d8e3a64554fdfbe360ad60c
Samuel Perreault <samuel.perreault.3@ulaval.ca>
2021-03-08 13:22:32 -0500

    revision for Ruiyang

--- application-NASDAQ100/functions/computeDiagTt_revised.R
+++ application-NASDAQ100/functions/computeDiagTt_revised.R
@@ -1,19 +1,24 @@
-computeTt <- function(new.Delta, Theta.hat, l.ij.mat){
+computeTt <- function(new.Delta, theta.hat, l.ij.mat){
   
   d <- dim(new.Delta)[1]
   p <- d * (d-1) / 2
   K <- dim(new.Delta)[2]
   
-  new.Theta <- Theta.hat
+  new.theta <- theta.hat
+  
   
   clusters <- c(tcrossprod(1:K,new.Delta))
-  clusters <- lapply(1:K, function(k){
-    which(clusters == k)
-  })
+  clus2 <- matrix(clusters[c(l.ij.mat)], ncol = 2)
+  clus2 <- t(apply(clus2, 1, sort))
+  clus1 <- clus2[,1]*(K+1) + clus2[,2]
+  uclus1 <- unique(clus1)
   
-  for(k in 1:length(clusters)){
-    new.Theta[clusters[[k]]] <- mean(Theta.hat[clusters[[k]]])
+  for(k in uclus1){
+    new.theta[clus1 == k] <- mean(theta.hat[clus1 == k])
   }
   
-  new.Theta
+  new.theta
 }
+
+#image(t(new.Theta[p:1,]), zlim = c(min(new.Theta[p:1,]),max(new.Theta[p:1,])), axes=FALSE, col=colfunc(100))
+#identical(new.Theta, Theta.list[[3]])
--- application-NASDAQ100/functions/pathBuilder0Para_revised.R
+++ application-NASDAQ100/functions/pathBuilder0Para_revised.R
@@ -21,23 +21,6 @@ pathBuilder0Para <- function(Tau.hat, Theta.hat, n, cutoff = NULL){
     c(i,j)
   }))
   
-  m.lr.mat <- matrix(rep(1:p,2),p,2)
-  
-  # lazy way to avoid errors in previously written code
-  lr.m.mat <- diag(1:p)
-  
-  m.shared.mat <- t(sapply(1:dim(m.lr.mat)[1], function(m){
-    shared <- intersect(l.ij.mat[m.lr.mat[m, 1],],l.ij.mat[m.lr.mat[m, 2],])
-    if(length(shared) == 0){
-      return(c(0,0))
-    }else if(length(shared) == 1){
-      return(c(0,shared))
-    }else{
-      shared <- sort(shared)
-      return(shared)
-    }
-  }))
-  
   #### Additional initial values ####
   
   Tau.list <- list()
@@ -49,7 +32,7 @@ pathBuilder0Para <- function(Tau.hat, Theta.hat, n, cutoff = NULL){
   
   Sigma.hat <- buildSigma(Theta.hat, tau.hat, n)
   Sigma.list[[1]] <- Sigma.hat
-
+  
   S <- Sigma.list[[1]]
   Si <- S^(-1)
   
@@ -85,7 +68,7 @@ pathBuilder0Para <- function(Tau.hat, Theta.hat, n, cutoff = NULL){
                             t(combn(which(clusters == k),2))
                           }, simplify = FALSE))
       L <- nrow(ij.pairs)
-    
+      
     }else{
       L <- (K*(K-1)/2)
       ij.pairs <- t(sapply(1:L,function(l){
@@ -94,7 +77,7 @@ pathBuilder0Para <- function(Tau.hat, Theta.hat, n, cutoff = NULL){
         c(i,j)
       }))
     }
-
+    
     sapply(1:L, function(l){
       pair <- ij.pairs[l,]
       Delta[,pair[1]] <- rowSums(Delta[,pair])
@@ -106,43 +89,36 @@ pathBuilder0Para <- function(Tau.hat, Theta.hat, n, cutoff = NULL){
     # Avoiding using Moore-Penrose for real.
     Delta2 <- tcrossprod(Delta)
     tau.tilde <- ((Delta2 %*% (Tau.hat - diag(d)) %*% Delta2) / (Delta2 %*% (1 - diag(d)) %*% Delta2))[l.ij.mat]
-    vec <- tau.hat - tau.tilde
+    vec <- Tau.hat[l.ij.mat] - tau.tilde
     crossprod(vec^2, Si)
   }
   
   #### Iterations ####
   # i <- 3
-
+  
   for(i in 2:(d-1)){
     print(paste0("Hey! Now at iteration ", i))
     Delta.candidates <- DeltaCandidates(Delta.list[[i-1]])
-
+    
     # We parallel the computations for the potential losses
     cl <- makeCluster(detectCores()-1)
     t1 <- Sys.time()
     print("export")
     # Should definitively let the workers construct the candidates (they need) - to do later, or never.
     # The bottleneck clearly this next line.
-    clusterExport(cl, c("Delta.candidates", "Tau.hat", "Si", "l.ij.mat", "d"), envir = environment())
+    clusterExport(cl, c("Delta.candidates", "Tau.hat", "Si", "l.ij.mat", "d", "loss", "tau.hat"), envir = environment())
     t2 <- Sys.time()
     print(difftime(t2,t1))
-
-    print("comp")
-    t1 <- Sys.time()
+    
     new.Delta <- Delta.candidates[[which.min(parSapply(cl, Delta.candidates, function(Delta){loss(Delta, Tau.hat, Si, l.ij.mat, d)}))]]
-    t2 <- Sys.time()
-    print(difftime(t2,t1))
     
     stopCluster(cl)
     
     print("rest")
-    t1 <- Sys.time()
     Delta2 <- tcrossprod(new.Delta)
     tau.tilde <- ((Delta2 %*% (Tau.hat - diag(d)) %*% Delta2) / (Delta2 %*% (1 - diag(d)) %*% Delta2))[l.ij.mat]
-    Theta.tilde <- computeTt(new.Delta, Theta.hat, m.lr.mat, lr.m.mat, m.shared.mat, l.ij.mat)
+    Theta.tilde <- computeTt(new.Delta, Theta.hat, l.ij.mat)
     Sigma.list[[i]] <- buildSigma(Theta.tilde, tau.tilde, n)
-    t2 <- Sys.time()
-    print(difftime(t2,t1))
     
     S <- Sigma.list[[i]]
     Si <- S^(-1)
@@ -154,7 +130,7 @@ pathBuilder0Para <- function(Tau.hat, Theta.hat, n, cutoff = NULL){
   new.Delta <- matrix(1, d, 1)
   Delta2 <- matrix(1,d,d)
   tau.tilde <- ((Delta2 %*% (Tau.hat - diag(d)) %*% Delta2) / (Delta2 %*% (1 - diag(d)) %*% Delta2))[l.ij.mat]
-  Theta.tilde <- computeTt(new.Delta, Theta.hat, m.lr.mat, lr.m.mat, m.shared.mat, l.ij.mat)
+  Theta.tilde <- computeTt(new.Delta, Theta.hat, l.ij.mat)
   Sigma.list[[d]] <- buildSigma(Theta.tilde, tau.tilde, n)
   
   Delta.list[[d]] <- new.Delta
